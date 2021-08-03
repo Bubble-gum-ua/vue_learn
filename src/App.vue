6450f4a28e591898c831a6c94496fedce3507162
bubble <bubble.gumgum1992@gmail.com>
2021-08-04 00:07:30 +0300

    added items to LS

--- src/App.vue
+++ src/App.vue
@@ -192,9 +192,45 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      coinsList: [],
     };
   },
+/*  created: async function () {
+    await fetch(
+      `  https://min-api.cryptocompare.com/data/all/coinlist?summary=true`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.coinsList = data.Data;
+      });
+  } ,*/
+  created() {
+    const tickersData = localStorage.getItem("cryptonomicon-list")
+    if(tickersData){
+      this.tickers = JSON.parse(tickersData)
+      this.tickers.forEach((ticker) => {
+          this.subscribeToUpdates(ticker.name)
+      })
+    }
+  },
+
   methods: {
+    subscribeToUpdates(tickerName){
+      setInterval(async () => {
+        const f = await fetch(
+            `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=10b7215deb51f422cadae475e1a2ff8a7c84f327cd1898f6d78f3924f4477e8b`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === tickerName).price =
+            data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
+    },
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -203,18 +239,8 @@ export default {
       if (this.ticker.length > 0) {
         this.tickers.push(currentTicker);
       }
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=10b7215deb51f422cadae475e1a2ff8a7c84f327cd1898f6d78f3924f4477e8b`
-        );
-        const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 5000);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers))
+      this.subscribeToUpdates(currentTicker.name)
       this.ticker = "";
     },
     handleDelete(tickerToRemove) {
@@ -234,4 +260,3 @@ export default {
   },
 };
 </script>
-
